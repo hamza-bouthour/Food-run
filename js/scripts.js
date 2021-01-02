29a79ac1990160bd20d131327a44d262106e9ea3
Hamza Bouthour <h.bouthour@gmail.com>
2021-01-02 11:13:12 -0800

    js added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 const popularDishes = {
-    ChickenCordonBleu: {
+    chickenCordonBleu: {
         name: 'Chicken Cordon Bleu',
         cost: 7,
         time: 120,
@@ -28,10 +28,77 @@ const popularDishes = {
             cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
             breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
         }
-
-
-
-    }
+    },
+    // bakedchicken: {
+    //     name: 'Baked Chicken',
+    //     cost: 10,
+    //     time: 25,
+    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
+    //     products: {
+    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
+    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
+    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
+    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
+    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
+    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
+    //     }
+    // },
+    // bakedchicken: {
+    //     name: 'Baked Chicken',
+    //     cost: 10,
+    //     time: 25,
+    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
+    //     products: {
+    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
+    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
+    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
+    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
+    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
+    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
+    //     }
+    // },
+    // bakedfish: {
+    //     name: 'Baked Chicken',
+    //     cost: 10,
+    //     time: 25,
+    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
+    //     products: {
+    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
+    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
+    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
+    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
+    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
+    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
+    //     }
+    // },
+    // bakedbeef: {
+    //     name: 'Baked Chicken',
+    //     cost: 10,
+    //     time: 25,
+    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
+    //     products: {
+    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
+    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
+    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
+    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
+    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
+    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
+    //     }
+    // },
+    // bakeddik: {
+    //     name: 'Baked Chicken',
+    //     cost: 10,
+    //     time: 25,
+    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
+    //     products: {
+    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
+    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
+    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
+    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
+    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
+    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
+    //     }
+    // },
 }
 
 const ingredientsProductImages = {
@@ -46,16 +113,16 @@ const ingredientsProductImages = {
     butter: { img: 'https://i.postimg.cc/xjBKWzX3/butter.jpg', price: 3.42 }
 }
 
-function hamsa() {
+function renderPopularList() {
     Object.keys(popularDishes).forEach(function(key) {
-
-
-
-
-
+        // popularDishes[key].map(function(i) {
+        //     console.log(i)
+        // })
+        // let idDish = key;
+        let idModal = key + "Modal"
+        
         $("div#popular-dishes-container").append(
-            `
-                <div class="col-6 col-md-4 col-lg-3">
+            `   <div class="col-6 col-md-4 col-lg-3">
                     <div class="card mb-3">
                         <a href=""><img src="${popularDishes[key].img}" alt="" class="card-img-top zoom"></a>
                         <div class="card-body">
@@ -64,41 +131,66 @@ function hamsa() {
                         </div>
                         <button role="button" class="btn products-button text-white" data-toggle="modal" data-target="#${key}">Products</button>
                     </div>
-                </div>
-    
+                </div>    
+          
+            ` 
+            )
+            $("#modalsContainer").append(
+                `
                 <div class="modal fade" id="${key}" role="dialog">
                     <div class="modal-dialog modal-lg" role="document">
-                        <div class="modal-content">
+                        <div class="modal-content" id="${idModal}">
                             <div class="modal-header bg-info text-white">
-                                <h3 class="modal-title">Products</h3>
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h3 class="modal-title">Products</h3>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                             </div>
                             <div class="modal-body">
                                 <div class="row">
-                                    <div class="col-6 col-md-4 col-lg-3">
-                                        <div class="card mb-3">
-                                            <a href=""><img src="" alt="" class="card-img-top zoom"></a>
-                                            <div class="card-body">
-                                                <h5 class="card-title">${popularDishes[key].name}<span class="card-text offset-4">1.99$</span></h5>
-                                                <p class="card-text">Hello dfsadfsafdsafsadfsfasfasfas<span class="offset-1 card-text"></span></p>
-                                            </div>
-                                            <button role="button" class="btn btn-info" data-toggle="modal" data-target="#${key}">Products</button>
-                                        </div>
-                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            `
-        )
+                `
+               
+            )
+            
+        
+                
+            Object.keys(popularDishes[key].products).map(function(item) {
+                    $('#chickenCordonBleuModal').children("div.modal-body").children('div.row').append(`
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <div class="card mb-3">
+                                <a href=""><img src="${popularDishes[key].products[item].img}" alt="" class="card-img-top zoom"></a>
+                                <div class="card-body">
+                                    <h5 class="card-title"><span class="card-text offset-4">1.99$</span></h5>
+                                    <p class="card-text">Hello dfsadfsafdsafsadfsfasfasfas<span class="offset-1 card-text"></span></p>
+                                </div>
+                                <button role="button" class="btn btn-info" data-toggle="modal" data-target="#${key}">Products</button>
+                            </div>
+                        </div>
+                    `)
+          
+                    // console.log(popularDishes[key].products[item].img)
+               })   
+        
 
-
-
-    });
+    })
 }
 
+// function renderModal() {
+//     Object.keys(popularDishes).forEach(function(key) {
 
+//     })
+// }
+      // $(idModal).addClass("d-none");
+                // $(idModal).css("background-color", "red");
+        
+           
+            // alert('dfs')
+                // console.log(idModal)
+                    
 // const displayDishes = (dishesList) => {
 //     let dishes = Object.keys(dishesList);
 //     dishes.forEach(function(item, index, array) {
@@ -112,4 +204,4 @@ function hamsa() {
 
 
 // displayDishes(popularDishes);
-hamsa();
+renderPopularList();
