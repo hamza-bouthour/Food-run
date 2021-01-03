6be2d0bf8eebc02ef1b8b046b36d4cf2135c2528
Hamza Bouthour <h.bouthour@gmail.com>
2021-01-02 19:40:23 -0800

    functionality added

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,67 @@
 const popularDishes = {
-    chickenCordonBleu: {
-        name: 'Chicken Cordon Bleu',
-        cost: 7,
-        time: 120,
-        img: 'https://i.postimg.cc/gc3TtmJj/chicken.png',
-        products: {
-            bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
-            salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
-            blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
-            swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
-            cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
-            breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
-        }
+    // chickenCordonBleu: {
+    //     name: 'Cordon Bleu',
+    //     cost: 7,
+    //     time: 120,
+    //     img: 'https://i.postimg.cc/GhQ3L4v6/chicken.jpg',
+    //     products: {
+    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94,name: "Boneless Chicken" },
+    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87,name: "Salt"  },
+    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18,name: "Black pepper"  },
+    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84,name: "Swiss cheese"  },
+    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97,name: "Cooked Ham"  },
+    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00,name: "Bread crumbs"  }
+    //     }
 
 
-    },
+    // },
     bakedSalmon: {
         name: 'Baked Salmon',
+        cost: 10,
+        time: 25,
+        img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
+        products: {
+            bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94,name: "Boneless Chicken" },
+            salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87,name: "Salt"  },
+            blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18,name: "Black pepper"  },
+            swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84,name: "Swiss cheese"  },
+            cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97,name: "Cooked Ham"  },
+            breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00,name: "Bread crumbs"  }
+        }
+
+    },
+    bakedchicken: {
+        name: 'Baked Chicken',
+        cost: 10,
+        time: 25,
+        img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
+        products: {
+            bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94,name: "Boneless Chicken" },
+            salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87,name: "Salt"  },
+            blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18,name: "Black pepper"  },
+            swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84,name: "Swiss cheese"  },
+            cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97,name: "Cooked Ham"  },
+            breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00,name: "Bread crumbs"  }
+        }
+
+    },
+    bakedchicken: {
+        name: 'Baked Chicken',
+        cost: 10,
+        time: 25,
+        img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
+        products: {
+            bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94,name: "Boneless Chicken" },
+            salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87,name: "Salt"  },
+            blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18,name: "Black pepper"  },
+            swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84,name: "Swiss cheese"  },
+            cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97,name: "Cooked Ham"  },
+            breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00,name: "Bread crumbs"  }
+        }
+
+    },
+    bakedfish: {
+        name: 'Baked Chicken',
         cost: 10,
         time: 25,
         img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
@@ -29,105 +74,49 @@ const popularDishes = {
             breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
         }
     },
-    // bakedchicken: {
-    //     name: 'Baked Chicken',
-    //     cost: 10,
-    //     time: 25,
-    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
-    //     products: {
-    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
-    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
-    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
-    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
-    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
-    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
-    //     }
-    // },
-    // bakedchicken: {
-    //     name: 'Baked Chicken',
-    //     cost: 10,
-    //     time: 25,
-    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
-    //     products: {
-    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
-    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
-    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
-    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
-    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
-    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
-    //     }
-    // },
-    // bakedfish: {
-    //     name: 'Baked Chicken',
-    //     cost: 10,
-    //     time: 25,
-    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
-    //     products: {
-    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
-    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
-    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
-    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
-    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
-    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
-    //     }
-    // },
-    // bakedbeef: {
-    //     name: 'Baked Chicken',
-    //     cost: 10,
-    //     time: 25,
-    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
-    //     products: {
-    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
-    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
-    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
-    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
-    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
-    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
-    //     }
-    // },
-    // bakeddik: {
-    //     name: 'Baked Chicken',
-    //     cost: 10,
-    //     time: 25,
-    //     img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
-    //     products: {
-    //         salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
-    //         bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
-    //         blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
-    //         swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
-    //         cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
-    //         breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
-    //     }
-    // },
+    bakedbeef: {
+        name: 'Baked Chicken',
+        cost: 10,
+        time: 25,
+        img: 'https://i.postimg.cc/15hZ4vgn/bakedsalmon.jpg',
+        products: {
+            salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
+            bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
+            blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
+            swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
+            cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
+            breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 }
+        }
+    },
 }
 
-const ingredientsProductImages = {
-    salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
-    bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
-    blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
-    swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
-    cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
-    breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 },
-    salmon: { img: 'https://i.postimg.cc/T153VhGR/salmon.jpg', price: 2.22 },
-    mustard: { img: 'https://i.postimg.cc/Jtw4Pm4j/mustard.jpg', price: 1.97 },
-    butter: { img: 'https://i.postimg.cc/xjBKWzX3/butter.jpg', price: 3.42 }
-}
+// const ingredientsProductImages = {
+//     salt: { img: 'https://i.postimg.cc/mr41T347/salt.png', price: 3.87 },
+//     bonelessChicken: { img: 'https://i.postimg.cc/jdJDN3dz/chicken.jpg', price: 5.94 },
+//     blackPepper: { img: 'https://i.postimg.cc/Z5tCLPCX/pepper.jpg', price: 3.18 },
+//     swissCheese: { img: 'https://i.postimg.cc/W1GtYDZz/cheese.jpg', price: 1.84 },
+//     cookedHam: { img: 'https://i.postimg.cc/FKhzM9bZ/ham.png', price: 3.97 },
+//     breadCrumbs: { img: 'https://i.postimg.cc/YS80kJbw/bread-crumbs.jpg', price: 8.00 },
+//     salmon: { img: 'https://i.postimg.cc/T153VhGR/salmon.jpg', price: 2.22 },
+//     mustard: { img: 'https://i.postimg.cc/Jtw4Pm4j/mustard.jpg', price: 1.97 },
+//     butter: { img: 'https://i.postimg.cc/xjBKWzX3/butter.jpg', price: 3.42 }
+// }
 
 function renderPopularList() {
     Object.keys(popularDishes).forEach(function(key) {
-        // popularDishes[key].map(function(i) {
-        //     console.log(i)
-        // })
-        // let idDish = key;
         let idModal = key + "Modal"
         
         $("div#popular-dishes-container").append(
             `   <div class="col-6 col-md-4 col-lg-3">
-                    <div class="card mb-3">
-                        <a href=""><img src="${popularDishes[key].img}" alt="" class="card-img-top zoom"></a>
+                    <div class="card mb-3" style="border-radius: 5px">
+                        <a href=""><img src="${popularDishes[key].img}" alt="" class="card-img-top img-thumbnail" ></a>
                         <div class="card-body">
-                            <h5 class="card-title">${popularDishes[key].name}<span class="card-text offset-4">1.99$</span></h5>
-                            <p class="card-text">Hello dfsadfsafdsafsadfsfasfasfas<span class="offset-1 card-text"></span></p>
+                            <h5 class="card-title text-info">${popularDishes[key].name}</h5>
+                            
+                            <div>
+                            <p><small>Cooking: </small><span class="card-text"><b>${popularDishes[key].time}min </b><i class="far fa-clock"></i></span></p>
+                            <p><small>Cost: </small><span class="card-text"><b>${popularDishes[key].cost}</b><i class="fas fa-dollar-sign"></i></span></p>
+                            </div>
                         </div>
                         <button role="button" class="btn products-button text-white" data-toggle="modal" data-target="#${key}">Products</button>
                     </div>
@@ -141,8 +130,16 @@ function renderPopularList() {
                     <div class="modal-dialog modal-lg" role="document">
                         <div class="modal-content" id="${idModal}">
                             <div class="modal-header bg-info text-white">
-                                    <h3 class="modal-title">Products</h3>
-                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <div class="container">
+                                    <div class="row">
+                                        <h3 class="modal-title col-6">Products</h3>
+                                        <div class="display-cart-in-modal offset-3 my-auto">
+                                        <i class="fas fa-3x fa-clipboard-list"></i>
+                                        <span class="display-list-number-items"></span>
+                                        </div>
+                                        <button type="button" class="close my-auto" data-dismiss="modal">&times;</button>
+                                    </div>
+                                </div>
                             </div>
                             <div class="modal-body">
                                 <div class="row">
@@ -159,15 +156,16 @@ function renderPopularList() {
         
                 
             Object.keys(popularDishes[key].products).map(function(item) {
-                    $('#chickenCordonBleuModal').children("div.modal-body").children('div.row').append(`
+                    $("#" + idModal).children("div.modal-body").children('div.row').append(`
                         <div class="col-6 col-md-4 col-lg-3">
                             <div class="card mb-3">
                                 <a href=""><img src="${popularDishes[key].products[item].img}" alt="" class="card-img-top zoom"></a>
                                 <div class="card-body">
-                                    <h5 class="card-title"><span class="card-text offset-4">1.99$</span></h5>
-                                    <p class="card-text">Hello dfsadfsafdsafsadfsfasfasfas<span class="offset-1 card-text"></span></p>
+                                    <h5 class="card-title"><span class="card-text">${popularDishes[key].products[item].name}</span></h5>
+                                    <strong class="card-text">${popularDishes[key].products[item].price} <i class="fas fa-dollar-sign"></i></strong>
                                 </div>
-                                <button role="button" class="btn btn-info" data-toggle="modal" data-target="#${key}">Products</button>
+                                <button role="button" class="btn products-button text-white add-list-btn">Add to List</button>
+                                <button role="dismiss" class="btn remove-button text-white d-none">Remove</button>
                             </div>
                         </div>
                     `)
@@ -179,29 +177,27 @@ function renderPopularList() {
     })
 }
 
-// function renderModal() {
-//     Object.keys(popularDishes).forEach(function(key) {
-
-//     })
-// }
-      // $(idModal).addClass("d-none");
-                // $(idModal).css("background-color", "red");
-        
-           
-            // alert('dfs')
-                // console.log(idModal)
-                    
-// const displayDishes = (dishesList) => {
-//     let dishes = Object.keys(dishesList);
-//     dishes.forEach(function(item, index, array) {
-//         let zeb = dishesList.item;
-//         console.log(dishesList.dishes)
-//     })
-
-//     asba = dishes[0]
-//     console.log(asba)
-// }
-
-
-// displayDishes(popularDishes);
+function displayListInModal(numbers) {
+    let spanList = $("span.display-list-number-items")
+    // let numbers = 0;
+    $(spanList).text(numbers);
+}
+$(document).ready(function() {
+    let number = 0
+    $("button.add-list-btn").click(function() {
+        $(this).parent().addClass("checked-div");
+        $(this).siblings().removeClass("d-none");
+        $(this).addClass("d-none");
+        number++;
+        displayListInModal(number);
+    })
+    $("button.remove-button").click(function() {
+        $(this).parent().removeClass("checked-div");
+        $(this).siblings().removeClass("d-none");
+        $(this).addClass("d-none");
+        number--;
+        displayListInModal(number);
+    })
+})
 renderPopularList();
+displayListInModal();
